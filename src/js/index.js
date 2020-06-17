e9c8e5d17b2a75c960b346416c17195bf05c3148
Jakubszy <psszydlos@gmail.com>
2020-06-17 17:34:38 +0200

    Task 1/1

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,68 @@
-const popup = document.querySelector('.popup');
+const popup = document.querySelector(".popup");
 
-const btnClickMe = document.querySelector('.btn');
-const btnClosePopup = document.querySelector('.popup .fa-times-circle');
-const btnSubmit = document.querySelector('.submit');
+const btnClickMe = document.querySelector(".btn");
+const btnClosePopup = document.querySelector(".popup .fa-times-circle");
+const btnSubmit = document.querySelector(".submit");
 
-const inputPassword = document.querySelector('.password');
-const InputEmail = document.querySelector('.email');
-const checkbox = document.querySelector('#checkbox');
+const inputPassword = document.querySelector(".password");
+const InputEmail = document.querySelector(".email");
+const checkbox = document.querySelector("#checkbox");
+
+const sukcessMsg = document.querySelector(".sukcessMsg");
 
 let FLAGemail = true;
 let FLAGpassword = true;
 let FLAGcheckbox = true;
 
-InputEmail.addEventListener("input", function () {
-    const emailReq =
-        /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/;
-    (!emailReq.test(this.value) ? FLAGemail = true : FLAGemail = false)
-    console.log(FLAGemail)
-})
-
-inputPassword.addEventListener("input", function () {
-    // I assume that the password must contain at least 5 characters.
-    if (this.value.length >= 5) {
-        FLAGpassword = false;
-    } else {
-        FLAGpassword = true;
-    }
+InputEmail.addEventListener("input", function() {
+  const emailReq = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/;
+  !emailReq.test(this.value) ? (FLAGemail = true) : (FLAGemail = false);
+  console.log(FLAGemail);
 });
 
-checkbox.addEventListener('input', function () {
-    if (checkbox.checked == true) {
-        FLAGcheckbox = false
-    } else {
-        FLAGcheckbox = true
-    }
-})
+inputPassword.addEventListener("input", function() {
+  // I assume that the password must contain at least 5 characters.
+  if (this.value.length >= 5) {
+    FLAGpassword = false;
+  } else {
+    FLAGpassword = true;
+  }
+});
 
-const submit = function () {
-    if ((FLAGemail === false) && (FLAGpassword === false) && (FLAGcheckbox === false)) {
-        btnSubmit.style.backgroundColor = '#fc888e';
-        console.log('All correct');
-    } else {
-        alert('Enter the email and password correctly and confirm the agreement to term & conditions.')
-    }
-}
+checkbox.addEventListener("input", function() {
+  if (checkbox.checked == true) {
+    FLAGcheckbox = false;
+  } else {
+    FLAGcheckbox = true;
+  }
+  console.log(FLAGcheckbox);
+});
 
-btnClickMe.addEventListener('click', function () {
-    popup.style.display = 'flex';
-})
+const submitAndEnd = function() {
+  popup.style.display = "none";
+  btnClickMe.style.display = "none";
+  sukcessMsg.classList.add("MsgActive");
+};
 
-btnClosePopup.addEventListener('click', function () {
-    popup.style.display = 'none';
-})
+const submit = function() {
+  if (FLAGemail === false && FLAGpassword === false && FLAGcheckbox === false) {
+    btnSubmit.style.backgroundColor = "#fc888e";
+    setTimeout(function() {
+      submitAndEnd();
+    }, 3000);
+  } else {
+    alert(
+      "Enter the email and password correctly and confirm the agreement to term & conditions."
+    );
+  }
+};
 
-btnSubmit.addEventListener('click', submit);
+btnClickMe.addEventListener("click", function() {
+  popup.style.display = "flex";
+});
+
+btnClosePopup.addEventListener("click", function() {
+  popup.style.display = "none";
+});
+
+btnSubmit.addEventListener("click", submit);
